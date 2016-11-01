9c7ac629b65c02a565ff4d03853c1e0f18b0d191
alebelcor <856838+alebelcor@users.noreply.github.com>
2016-11-01 16:36:36 -0700

    Add spellcheck
    Fix #2

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var jsonLint = require('gulp-jsonlint');
 var jsonSchema = require('gulp-json-schema');
+var run = require('gulp-run');
 
 gulp.task('validate', function () {
   return gulp.src('resume.json')
@@ -15,9 +16,19 @@ gulp.task('lint', function () {
     .pipe(jsonLint.reporter());
 });
 
+gulp.task('spellcheck', function (cb) {
+  run('yaspeller resume.json').exec()
+    .on('error', function (err) {
+      console.error(err.message);
+      cb();
+    })
+    .on('finish', cb);
+});
+
 gulp.task('test', [
   'lint',
-  'validate'
+  'validate',
+  'spellcheck'
 ]);
 
 gulp.task('default', [
